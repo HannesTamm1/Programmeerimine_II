e107f71d1fe5a2229dd63bf9933eba7a4fe4f9cb
Hannes Tamm <hannes.tamm@tptlive.ee>
2024-10-11 09:43:56 +0300

    tund 08.11
    Tund 25.10
    
    tund 1
    
    Updated classes
    
    Added classes
    
    Add files via upload
    
    Delete E-POOD TA_23A HANNES TAMM.png
    
    Delete E-POOD
    
    Add files via upload
    
    Create E-POOD

---
+++ KooliProjekt/Data/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace KooliProjekt.Data
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ KooliProjekt/Data/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace KooliProjekt.Data
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public Order Order { get; set; }
+        public int OrderId { get; set; }
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+        public float PriceAtOrder { get; set; }
+        public int Quantity { get; set; }
+    }
+}
