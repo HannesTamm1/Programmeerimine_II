5c14bcad0ca9a73ce99b9b74becc8cf434deb0af
50701030237 <50701030237@KLASSA302-16>
2024-10-18 11:03:40 +0300

    Updated classes

--- KooliProjekt/Data/Categories.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KooliProjekt.Data
-{
-    public class Categories
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-    }
-}
---
+++ KooliProjekt/Data/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace KooliProjekt.Data
+{
+    public class OrderItem
+    {
+            public int OrderItemId { get; set; }
+            public int OrderId { get; set; }
+            public int ProductId { get; set; }
+            public float PriceAtOrder { get; set; }
+            public int Quantity { get; set; }
+    }
+}
