77008ce9d7f82b287a5fa96b99e3b3605b7453ea
Hannes Tamm <hannes.tamm@tptlive.ee>
2024-11-16 13:11:12 +0200

    Reapply "tund 08.11"
    This reverts commit 8160787a5fecb651ed83508c7015bef6cf81f2d5.

---
+++ KooliProjekt/Data/user.cs
@@ -0,0 +1,10 @@
+﻿namespace KooliProjekt.Data
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
