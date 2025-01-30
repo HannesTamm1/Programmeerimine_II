8bfe810b77a0bd287066c3c794553fe2d61e9c70
50701030237 <50701030237@KLASSA302-16>
2025-01-30 15:20:32 +0200

    repos tests

---
+++ KooliProjekt.UnitTests/ServiceTests/OrderServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using KooliProjekt.Data;
+using KooliProjekt.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KooliProjekt.UnitTests.ServiceTests
+{
+    public class OrderServiceTests
+    {
+        private readonly Mock<IUnitofWork> _uowMock;
+        private readonly Mock<IOrderService> _repositoryMock;
+        private readonly OrderService _orderService;
+            
+
+        public OrderServiceTests() 
+        {
+            _uowMock = new Mock<IOrderService>();
+            _repositoryMock = new Mock<IOrderService>();
+            _orderService = new OrderService(_repositoryMock.Object); 
+        }
+        [Fact]
+        public async Task List_Should_return_list_of_cars()
+        {
+            // Arrange
+            var results = new PagedResult<Order>();
+        }
+    }
+}
