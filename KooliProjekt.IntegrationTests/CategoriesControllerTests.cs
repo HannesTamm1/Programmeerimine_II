97a1ab93989bf18f33c6f67b4507a816d8eb5bec
50701030237 <50701030237@KLASSA302-16>
2025-02-20 13:52:02 +0200

    Added missing integrationtests

---
+++ KooliProjekt.IntegrationTests/CategoriesControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using KooliProjekt.Data;
+using KooliProjekt.IntegrationTests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KooliProjekt.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class CategoriesControllerTests : TestBase
+    {
+        private readonly HttpClient _client;
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesControllerTests()
+        {
+            _client = Factory.CreateClient();
+            _context = (ApplicationDbContext)Factory.Services.GetService(typeof(ApplicationDbContext));
+        }
+
+        [Fact]
+        public async Task Index_should_return_correct_response()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Categories");
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_was_not_found()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Categories/Details/100");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Categories/Details/");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        [Fact]
+        public async Task Details_should_return_ok_when_list_was_found()
+        {
+            // Arrange
+            var list = new Category { Name = "List 1" };
+            _context.Categories.Add(list);
+            _context.SaveChanges();
+
+            // Act
+            using var response = await _client.GetAsync("/Categories/Details/" + list.Id);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
